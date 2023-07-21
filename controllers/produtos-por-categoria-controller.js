3842b649f8c6bcf5e5105c8d433a479bebb2f1e5
Guilherme <schx2learn@gmail.com>
2023-07-21 18:14:22 -0300

    Producs page is now using the database, and other general changes

---
+++ controllers/produtos-por-categoria-controller.js
@@ -0,0 +1,35 @@
+import { productServices } from "../services/product-services.js";
+
+const listaProdutosEl = document.querySelector("[data-product]");
+
+function mostrarProduto(produtoData) {
+    const produtoURL = produtoData.url;
+    const produtoNome = produtoData.name;
+    const produtoPrice = produtoData.price;
+    const produtoCodigo = produtoData.code;
+    // const produtoId = produtoData.id;
+    // const produtoCategoria = produtoData.category;
+    // const produtoDescricao = produtoData.description;
+
+    const produtoEl = document.createElement('li');
+    produtoEl.classList.add("produto__item");
+
+    produtoEl.innerHTML = `
+        <img class="produto__imagem" src="${produtoURL}" width="176" height="174">
+        <p class="produto__nome">${produtoNome}</p>
+        <p class="produto__preco">${produtoPrice}</p>
+        <p class="produto__codigo">${produtoCodigo}</p>
+    `;
+
+    return produtoEl;
+}
+
+async function mostrarProdutos() {
+    try {
+        const listaProdutosAPI = await productServices.listaProdutos();
+        listaProdutosAPI.forEach(produto => listaProdutosEl.appendChild(mostrarProduto(produto)))
+    } catch {
+        listaProdutosEl.innerHTML = `<h2 class="produtos__titulo">Não foi possível carregar a lista de produtos<h2>`;
+    }
+}
+
---
+++ services/product-services.js
@@ -0,0 +1,62 @@
+// GET
+
+async function listaProdutos() {
+    const listaProdutosRequest = await fetch("http://localhost:3000/produtos");
+    const listaProdutosResponse = await listaProdutosRequest.json();
+
+    return listaProdutosResponse;    
+}
+
+async function produto(id) {
+    const produtoRequest = await fetch(`http://localhost:3000/produtos/${id}`);
+    const produtoResponse = await produtoRequest.json();
+
+    return produtoResponse;
+}
+
+async function buscarProduto(termoDeBusca) {
+    const buscarRequest = await fetch(`http://localhost:3000/produtos?q=${termoDeBusca}`);
+    const buscarResponse = await buscarRequest.json();
+
+    return buscarResponse;
+}
+
+
+// POST
+
+async function adicionarProduto(produto) {
+    const adicionarProdutoRequest = await fetch("http://localhost:3000/produtos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            url: produto.url,
+            alt: produto.alt,
+            categoria: produto.categoria,
+            name: produto.name,
+            price: produto.price,
+            code: ("#" + (Math.floor(Math.random() * 100000))).padEnd(6, "0"),
+            description: produto.description
+        })
+    });
+
+    console.log(produto);
+
+    if (!adicionarProdutoRequest.ok) {
+        throw Error("Não foi possível adicionar o produto");
+    } else {
+        console.log(`Produto adicionado ${produto}`);
+    }
+
+    const adicionarProdutoResponse = await adicionarProdutoRequest.json();
+
+    return adicionarProdutoResponse;
+}
+
+export const productServices = {
+    listaProdutos,
+    produto,
+    buscarProduto,
+    adicionarProduto
+}
