c21beeb505602cac00e791577e0b2f0fc5744eba
Guilherme <schx2learn@gmail.com>
2023-07-21 12:10:04 -0300

    Reestructuring for adding a API database and make requests

--- scripts/mostra-pesquisa.js
+++ scripts/mostra-pesquisa.js
@@ -1,7 +1,7 @@
-const showPesquisa = document.querySelector('.cabecalho__botao-mostra-pesquisa');
+const mostraPesquisa = document.querySelector('.cabecalho__botao-mostra-pesquisa');
 const pesquisa = document.querySelector('.cabecalho__pesquisa--pequena-view-esconder');
 
-showPesquisa.addEventListener("click", () => {
+mostraPesquisa.addEventListener("click", () => {
     pesquisa.classList.toggle("cabecalho__pesquisa--pequena-view-esconder");
     pesquisa.classList.toggle("cabecalho__pesquisa--pequena-view-mostrar");
 });
